a7cfb4558f3acdbb0c5eb635b5b507b12bed53a9
Administrator <fatmasalhi101@gmail.com>
2024-08-29 19:22:56 +0100

    finished category functionnality

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import './Feed.css'
 import thumbnail1 from '../../assets/thumbnail1.png'
 import thumbnail2 from '../../assets/thumbnail2.png'
@@ -9,112 +9,46 @@ import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
 import { Link } from 'react-router-dom'
+import { API_KEY } from '../../data'
+import { valueConverter } from '../../data'
+import moment from 'moment'; 
 
-const Feed = (category) => {
+
+
+const Feed = ({category}) => {
+
+     const [data,setData]=useState([]);
   
-    const fetchData=
+       const fetchData= async()=>{
+        const video_List_url= `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY} `
+        await fetch(video_List_url)
+                   .then(response =>response.json())
+                   .then(data=>setData(data.items));
+                   
+       }
 
+       useEffect(()=>{   //every category selected will update the data state and the incoming array
+        fetchData();
+       },[category])
 
 
   return (
     <div className="feed">
-    <Link to ={`video/10/6276`} className='card'>
-        <img src={thumbnail1} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </Link>
-    <div className='card'>
-        <img src={thumbnail2} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail3} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail4} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail5} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail6} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail7} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail8} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail1} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail2} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail3} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail4} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail5} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail6} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail7} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-    <div className='card'>
-        <img src={thumbnail8} alt="" />
-        <h2>Best channel to learn coding that helps you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2days ago</p>
-    </div>
-   
+        {
+            data.map((item,index)=>{
+                return (
+
+                    <Link to ={`video/${item.snippet.categoryId}/${item.id}`} className='card'>
+                    <img src={item.snippet.thumbnails.medium.url} alt="" />
+                    <h2>{item.snippet.title}</h2>
+                    <h3>{item.snippet.channelTitle}</h3>
+                    <p>{valueConverter(item.statistics.viewCount)} &bull; {moment(item.snippet.publishedAt).fromNow()}</p>
+                    </Link>  
+                    
+                )
+            })
+        }
+    
     </div>
   )
 }
--- src/data.js
+++ src/data.js
@@ -1,1 +1,12 @@
 export const API_KEY='AIzaSyB4n7BM6FFF_UtZYGX6We3ES-wf9IhLjbE'
+
+
+ export const valueConverter = (value)=>{ //function to display views
+    if(value >1000000){
+        return  Math.floor(value/1000000)+'M';
+    }else if (value >= 1000){
+        return  Math.floor(value/1000)+'K';
+    }else{
+        return value;
+    }
+}
